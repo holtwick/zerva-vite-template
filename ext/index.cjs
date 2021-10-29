37b5746aa703efb7328b69bf65c9ed6d6ce7cf0e
Dirk Holtwick <dirk.holtwick@gmail.com>
2021-10-29 19:26:55 +0200

    All wrong...

---
+++ ext/index.cjs
@@ -0,0 +1,6 @@
+const { parseHTML } = require("zeed-dom")
+
+const x = Math.random()
+console.log("HELLO", parseHTML, x)
+
+module.exports = { x, y: 1 }
--- vite/vite.config.ts
+++ vite/vite.config.ts
@@ -1,21 +1,31 @@
 import { defineConfig } from "vite"
 import vue from "@vitejs/plugin-vue"
-import { resolve } from "path"
 // import { zerva } from "../zerva"
+import { toPath } from "zeed"
 import { viteZervaPlugin } from "zerva-vite-plugin"
 
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
     outDir: "zerva/www",
-    sourcemap: true,
+    sourcemap: true,        
+  },
+  esbuild: {
+
   },
   plugins: [
     //
     vue(),
     viteZervaPlugin(async () => {
-      const x = await import("../zerva")
-      console.log("HELLO", process.env.NODE_PATH, x)
+      // const x = await import(toPath("../ext/index.cjs"))
+
+      // Trick to force reload https://ar.al/2021/02/22/cache-busting-in-node.js-dynamic-esm-imports/
+      const x = await import(
+        toPath(`../ext/index.cjs`) + `?update=${Math.random()}}`
+      )
+
+      // const x = await import(toPath("../zerva/"))
+      console.log("HELLO FROM CONFIG", x)) // , process.env.NODE_PATH)
     }),
   ],
 })
